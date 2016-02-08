5d6c1e9045b36fcc498619803d635a1f956e4783
Nithyanandam <nithyanandamv@visualbi.com>
2016-02-08 11:33:22 +0530

    Auto build for relase

---
+++ build.py
@@ -0,0 +1,12 @@
+import shutil
+import json
+from pprint import pprint
+
+with open('Hide Posts/manifest.json') as data_file:
+    data = json.load(data_file)
+
+pprint(data)
+release_version = data["version"]
+destination="release/Release "+release_version+".zip"       
+
+shutil.make_archive(destination, "zip", "Hide Posts")
